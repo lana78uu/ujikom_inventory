aa4258e774a597af9e7dae87d9e391581fefbf57
ianaa <kuroozamkai0713@outlook.com>
2025-04-12 17:41:51 +0700

    data_produk.php

--- data_produk.php
+++ data_produk.php
@@ -9,7 +9,7 @@ include "koneksi.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Barang</title>
+    <title>Tambah Produk</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <style>
@@ -138,7 +138,7 @@ include "koneksi.php";
     </nav>
 </header>
     <div class="container mt-5">
-        <h2>Tambah Barang</h2>
+        <h2>Tambah Produk</h2>
         <?php if (isset($response)): ?>
             <div class="alert alert-info"><?= htmlspecialchars($response); ?></div>
         <?php endif; ?>
